52fac5f155c74b534646f1665e49814b6590117c
37 <asttrvm@gmail.com>
2024-06-29 19:22:03 -0300

    Added more things to do in comments.

--- Program.cs
+++ Program.cs
@@ -2,13 +2,23 @@
 // Heroes
 
 // • More monster types.
-// amarok, Bite, has the rotten side effect, chance to make the opponent skip a turn
-// new scratch attack not fully implemented,    done, fully implemented
+// amarok, Bite, has the rotten side effect, chance to make the opponent skip a turn done
+// new scratch attack not fully implemented,                                         done, fully implemented
 // Bite does not show for Amarok    fixed
 
+// Mylara, implemented Cannon of consolas  done
+// need to fix damage                      done
 
-// manticore, has a slice attack, 2 damage
-// evil robot, Smart Misile attack, that attacks at everyone in the party for 1 damage
+// there is a current bug going on where if I use MyLara and Skoring name (long name) the program has a problem
+// after that, just refactoring
+// and then I'm finally done
+
+// evil robot, Smart Misile attack, that attacks at everyone in the party for 1 damage done
+// added evil robot class                                                              done
+// added misile attack                                                                 done
+// need to add it to input data base pending                                           done
+// need to add data for it to damage each player in the party                          done
+// added the logic to damage all enemies in opponent party                             done
 
 Game game = new Game();
 game.Run();
