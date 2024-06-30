741bf0ba185eef6b5bfa94f5f4bd9aab32d24852
37 <asttrvm@gmail.com>
2024-06-30 03:52:26 -0300

    Removed comments of done tasks. Added more comments with tasks.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿// Add default name for computer as TrueProgrammer
-// Add better colors. One for each thing. No repeating.
-// after that, just refactoring
-// Console goes down a bit                                                 pseudo-fixed? hacky solution
+﻿
+// Add more chance for characters to heal
+// Use all characters created
+// Rundown at everything, refactoring
 
-// when the battle ends it doesn't get the chance to print log messages
+
 
 Game game = new Game();
 game.Run();
