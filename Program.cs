a85c4aa9a9f830860679dfd9575f8e1f8b0b1465
37 <asttrvm@gmail.com>
2024-06-30 01:09:51 -0300

    Removed comments from tasks done.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,4 @@
-﻿// there is a current bug going on where if I use MyLara and Skoring name (long name)
-// the program has a problem (there was no space)
-// add something that puts a WriteLine or puts cursor one down and to the beginning when that happens   done
-// The problem of MyLara names is due to how  my ":" spacing is calculated fixed,                       solved
-// added text wrapping for logger                                                                       done
-
-
-// Characters should heal more                      
-// Rot turn skip is not working as intended, turns skip but weirdly   solved done
-// Bug, end message is not displaying?                                solved done
-
-// Add default name for computer as TrueProgrammer
+﻿// Add default name for computer as TrueProgrammer
 // Add better colors. One for each thing. No repeating.
 // after that, just refactoring
 // Console goes down a bit                                                 pseudo-fixed? hacky solution
