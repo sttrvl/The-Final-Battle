c5971705f8f7c1bc9a29fc4604d6b49942530551
37 <asttrvm@gmail.com>
2024-07-01 00:40:11 -0300

    Added more tasks to do, as comments.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-﻿
-// we need to see a way to add SimulasSoup directly
+﻿// we need to see a way to add SimulasSoup directly   done
+// we figure out a way to pass turn to the party      done
+// or we directly add the first players from the file done
 // Rundown at everything, refactoring
-
 
 Game game = new Game();
 game.Run();
