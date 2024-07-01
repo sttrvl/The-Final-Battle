731c631efdb5ab9ecd2795a41dba0e9e8437177d
37 <asttrvm@gmail.com>
2024-06-30 22:44:05 -0300

    Added new comments of tasks to do.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿
-// Add more chance for characters to heal
-// Use all characters created
-// Rundown at everything, refactoring
+﻿// Add more chance for characters to heal                  done
+// Add a Thread.Sleep(500) for computer, and 0 for player  done
+// Use all characters created                              done
+// the issue is we need add items to additional parties    done
+// We need to add items and gear by file                   done
+// SimulasSoup full heal                                   done
+// Gear is overflowing, we can make a sort of page number, if the gear overflows we just "advance" it done
+// Or we could simply remove it, or have a max                                           discarded, just displayed it differently
 
+// we need to see a way to add SimulasSoup directly
+// Rundown at everything, refactoring
 
 
 Game game = new Game();
