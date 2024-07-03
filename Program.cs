30df3217874f2404176ca20a9ecc74a931684773
37 <asttrvm@gmail.com>
2024-07-03 17:19:46 -0300

    Added comments of tasks to do.

--- Program.cs
+++ Program.cs
@@ -5,3 +5,28 @@
 
 Game game = new Game();
 game.Run();
+
+// things that are repeated a lot 1, 22 cursor position done
+// death and sick characters should be removed if death, since the message sometimes displays after death done, fixed
+// still ongoing for plague sickness            seems to be fixed
+// plague sickness should not be stacked        fixed
+// CheckPlague effects, turn not skipping       fixed
+// replace all Input manager for new InputManager() since instances do not matter     done
+
+// some characters still survive after 0 health 
+
+// I need to add like something to put automatically spaces in display maybe
+
+// character            done
+// gameobjects          done
+// partymanager         done
+// game                 done
+// inputmanager         done
+// turnmanager          checking
+// displayinformation   done organized
+
+
+// when a dagger gets added, the dagger attack displays first when it should be last
+// cursor goes down, we could force it to stay in place
+
+// Check if everything is where It's supposed to be living in
