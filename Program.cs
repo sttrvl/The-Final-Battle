a9e40665a1b3cca155366f4d2e0cd953aa0019b8
37 <asttrvm@gmail.com>
2024-07-04 01:10:20 -0300

    Removed old comments. Added more comments of things to do.

--- Program.cs
+++ Program.cs
@@ -8,21 +8,10 @@ game.Run();
 // death and sick characters should be removed if death, since the message sometimes displays after death done, fixed
 // still ongoing for plague sickness            seems to be fixed
 
-// when a dagger gets added, the dagger attack displays first when it should be last
-// cursor goes down, we could force it to stay in place
-// some characters still survive after 0 health 
+// when a dagger gets added, the dagger attack displays first when it should be last done
+// I need to add like something to put automatically spaces                          done
+// cursor goes down, we could force it to stay in place                              scrapped idea
 
-// I need to add like something to put automatically spaces in display maybe, too complicated?
-// if 0 souls do not display the gauge fill fixed, done
-
-// re order events done
-// add methods to modify the structs instead of doing it directly done
+// some characters still survive after 0 health                     
 
 // check parameters for everything
-
-// partymanager         done, organize, added structs , helper methods done (they are records, change to struct)
-//                            now they are classes, done
-// turnmanager          done organized
-// game                 done
-// inputmanager         done
-// displayinformation   done organized
