cb18734226fd687d8f5aaa74ef4589728186a94d
37 <asttrvm@gmail.com>
2024-07-04 19:44:39 -0300

    Removed old comments.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,4 @@
 
 Game game = new Game();
 game.Run();
-// death and sick characters should be removed if death, since the message sometimes displays after death done, fixed
-// still ongoing for plague sickness                                                                seems to be fixed
-
 // some characters still survive after 0 health                                                     seems to be fixed      
