29ca8bf5c7aa9f6f96caa9488cda502c7635426c
37 <asttrvm@gmail.com>
2024-07-04 01:11:34 -0300

    Removed old comments.

--- Program.cs
+++ Program.cs
@@ -8,10 +8,7 @@ game.Run();
 // death and sick characters should be removed if death, since the message sometimes displays after death done, fixed
 // still ongoing for plague sickness            seems to be fixed
 
-// when a dagger gets added, the dagger attack displays first when it should be last done
-// I need to add like something to put automatically spaces                          done
-// cursor goes down, we could force it to stay in place                              scrapped idea
-
+// encapsulate spacing method added in DisplayInformation
 // some characters still survive after 0 health                     
 
 // check parameters for everything
